7d370836e7d7082774fb7d8426f2375566745138
Internaut007 <70135745+internauten@users.noreply.github.com>
2026-03-19 10:50:34 +0100

    Erster Release!

--- internautenimage/internautenimage.php
+++ internautenimage/internautenimage.php
@@ -9,7 +9,7 @@ class InternautenImage extends Module
     {
         $this->name = 'internautenimage';
         $this->tab = 'administration';
-        $this->version = '0.1.0';
+        $this->version = '1.0.0';
         $this->author = 'Internauten';
         $this->need_instance = 0;
         $this->bootstrap = true;
